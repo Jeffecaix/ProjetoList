fefe14aa7ad1649d95cac4821a7d11d02ded711d
Jefferson Cais <73970429+Jeffecaix@users.noreply.github.com>
2022-02-09 19:18:25 -0300

    Add files via upload

---
+++ Desafio_3/Desafio_3/Documento/Classe/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_3.Documento.Classe
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {this.Nome}, Idade: {this.Idade}";
+        }
+
+    }
+}
---
+++ Desafio_3/Desafio_3/Program.cs
@@ -0,0 +1,61 @@
+﻿using Desafio_3.Documento.Classe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static List<Pessoa> pessoas;
+
+        static void Main(string[] args)
+        {
+            pessoas = new List<Pessoa>();
+
+            Pessoa joao = new Pessoa("Leandro", 21);
+            Pessoa Leandro = new Pessoa("João", 15);
+            Pessoa Paulo = new Pessoa("Paulo", 17);
+            Pessoa Jessica = new Pessoa("Jessica", 18);
+
+            pessoas.Add(joao);
+            pessoas.Add(Leandro);
+            pessoas.Add(Paulo);
+            pessoas.Add(Jessica);
+
+            foreach (Pessoa item in pessoas)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Lista iniciou com {pessoas.Count()}.");
+
+            pessoas.Remove(Leandro);
+            pessoas.Remove(Paulo);
+
+            Console.WriteLine($"Lista terminou com {pessoas.Count()}.");
+
+            // Consulta
+            var resultado = from nome in pessoas
+                            where nome.Nome == "Jessica"
+                            select nome;
+
+            int maisVelho = pessoas.Max(x => x.Idade);
+
+            var resultado1 = from nome in pessoas
+                             where nome.Idade == maisVelho
+                             select nome;
+
+            foreach (var result in resultado)
+            {
+                Console.WriteLine($"Nome: {result.Nome}, encontrado.");
+            }
+
+            foreach (var result in resultado1)
+            {
+                Console.WriteLine($"O nome da pessoa mais velha é {result.Nome}.");
+            }
+
+        }
+    }
+}
